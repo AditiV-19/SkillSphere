b88f6de6aae0110d2d183266fe4bf964d8a4dde8
AditiV-19 <aditiverma9d@gmail.com>
2026-06-26 01:22:36 +0530

    Added Role Based Access Control API

---
+++ backend/src/middleware/role.middleware.js
@@ -0,0 +1,31 @@
+export const freelancerOnly = (req, res, next) => {
+
+    if (!req.user) {
+            return res.status(401).json({
+                message: "Not authenticated"
+            });
+    }
+
+    if(req.user.role !== 'freelancer'){
+        res.status(403).json({ message: 'Only freelancers can access this route' })
+    }
+    
+    next();
+
+}
+
+export const clientOnly = (req, res, next) => {
+    
+    if (!req.user) {
+        return res.status(401).json({
+            message: "Not authenticated"
+        });
+    }
+
+    if(req.user.role !== 'client'){
+        res.status(403).json({ message: 'Only clients can access this route' })
+    }
+
+    next();
+
+}
--- backend/src/routes/profile.route.js
+++ backend/src/routes/profile.route.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { authMiddleware } from '../middleware/auth.middleware.js';
 import { createProfile, getProfile, updateProfile } from '../controller/profile.controller.js';
+import { clientOnly, freelancerOnly } from '../middleware/role.middleware.js';
 
 
 
@@ -11,4 +12,14 @@ router.post("/", authMiddleware, createProfile);
 router.get("/", authMiddleware, getProfile);
 router.put("/", authMiddleware, updateProfile);
 
+router.get("/freelancer-test", authMiddleware, freelancerOnly, (req, res)=> {
+    res.json({
+        message: 'Welcome Freelancer'
+    })
+});
+router.get("/client-test", authMiddleware, clientOnly, (req, res)=> {
+    res.json({
+        message: 'Welcome Client'
+    })
+});
 export default router;
