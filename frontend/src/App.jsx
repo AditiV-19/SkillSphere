f6f55c1f5503b18b94e9811fd89c19747d0c9a29
AditiV-19 <aditiverma9d@gmail.com>
2026-06-28 01:10:32 +0530

    Replaced AppRoutes.jsx with App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
-import Register from './pages/Register'
+import { Routes, Route } from "react-router-dom"
 
-export default function App() {
-  return (
-    <div>
-      <Register />
-    </div>
-  )
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Dashboard from  "./pages/Dashboard"
+import Profile from "./pages/Profile"
+import VerifyEmail from "./pages/VerifyEmail"
+
+export default function App(){
+
+    return(
+        <Routes>
+
+            <Route
+                path="/"
+                element={<Home/>}
+            />
+            <Route
+                path="/register"
+                element={<Register/>}
+            />
+            <Route
+                path="/login"
+                element={<Login/>}
+            />
+            <Route
+                path="/dashboard"
+                element={<Dashboard/>}
+            />
+            <Route
+                path="/profile"
+                element={<Profile/>}
+            />
+            <Route
+                path="/verify-email/:token"
+                element={<VerifyEmail/>}
+            />
+        </Routes>
+
+    )
 }
