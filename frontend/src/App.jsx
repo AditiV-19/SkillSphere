9c1921a1f5d826367f5c49dc15d0e681f1a974ac
AditiV-19 <aditiverma9d@gmail.com>
2026-06-30 23:45:28 +0530

    updated App.jsx routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom"
 
 import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Dashboard from  "./pages/Dashboard"
+import Login from "./pages/auth/Login"
+import Register from "./pages/auth/Register"
+import Dashboard from  "./components/dashboard/DashboardLayout"
 import Profile from "./pages/Profile"
-import VerifyEmail from "./pages/VerifyEmail"
+import VerifyEmail from "./pages/auth/VerifyEmail"
 import ProtectedRoute from './components/ProtectedRoute'
+import FreelancerDashboard from './pages/freelancer/FreelancerDashboard'
+import ClientDashboard from './pages/client/ClientDashboard' 
+import AdminDashboard from './pages/admin/AdminDashboard'
 
 export default function App(){
 
@@ -44,6 +47,37 @@ export default function App(){
             <Route
                 path="/verify-email/:token"
                 element={<VerifyEmail/>}
+            />
+            
+            <Route
+                path="/freelancer/dashboard"
+                element={
+                    <ProtectedRoute
+                allowedRoles={["freelancer"]}
+            >
+                <FreelancerDashboard />
+            </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/client/dashboard"
+                element={
+                    <ProtectedRoute
+                        allowedRoles={["client"]}
+                    >
+                        <ClientDashboard />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/admin/dashboard"
+                element={
+                    <ProtectedRoute
+                        allowedRoles={["admin"]}
+                    >
+                        <AdminDashboard />
+                    </ProtectedRoute>
+                }
             />
         </Routes>
 
